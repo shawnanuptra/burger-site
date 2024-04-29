5bd0532cc079a089baadba2cd8fe28941d397ba3
Shawn Martin <bh74sa@student.sunderland.ac.uk>
2024-04-29 16:31:06 +0100

    changed fonts added nav item animations changed hero styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 import HeroImage from "./assets/hero.jpg";
+
 const NavBar = styled.nav`
-	background-color: #ef491f;
+	background-color: #ff8c49;
 	text-transform: uppercase;
 	font-weight: 900;
 	height: 6rem;
@@ -21,7 +22,10 @@ const NavBar = styled.nav`
 		margin: 0 auto 0 0;
 		.logo {
 			font-size: 2rem;
-			font-weight: bolder;
+			font-weight: 700;
+			font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
+			letter-spacing: 3px;
+			cursor: pointer;
 		}
 	}
 
@@ -36,12 +40,27 @@ const NavBar = styled.nav`
 			gap: 1rem;
 			margin: 0;
 			padding: 0;
+
+			/* list items */
+			li {
+				border: 3px solid transparent;
+				border-radius: 5px;
+				padding: 10px 15px;
+				transition: all 0.1s ease-out;
+
+				&:hover {
+					border: 3px solid black;
+					background-color: white;
+					cursor: pointer;
+					filter: drop-shadow(5px 5px 0px #000000);
+				}
+			}
 		}
 	}
 `;
 
 const Hero = styled.main`
-	background-color: #ef491f;
+	background-color: #ff8c49;
 	margin-top: 5rem;
 	.hero-wrapper {
 		max-width: 1100px;
@@ -64,32 +83,40 @@ const Hero = styled.main`
 			p {
 				max-width: 500px;
 				margin: 0;
-				font-weight: 700;
+				font-weight: 500;
 				margin-top: 2rem;
 				font-size: 1.2rem;
 			}
 			h1 {
-				font-size: 5rem;
-				font-weight: 900;
+				font-size: 6rem;
+				font-weight: 800;
+				font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
 				margin: 0;
 				line-height: 1.1;
 				text-transform: uppercase;
+				color: #ffed49;
+				-webkit-text-stroke: 4px black;
+				text-shadow: 8px 6px black;
 			}
 
 			.hero-cta {
 				text-decoration: none;
-				color: white;
+				color: black;
 				text-transform: uppercase;
-				background-color: black;
+				background-color: #ffed49;
 				padding: 1rem 2rem;
 				width: fit-content;
 				margin-top: 3rem;
-				font-weight: 700;
+				font-weight: 600;
+				font-size: 1.2rem;
+				letter-spacing: 0.1ch;
 				border-radius: 0.5rem;
 				border: 5px solid black;
 				transition: all 0.1s ease-out;
+				filter: drop-shadow(5px 5px 0px #000000);
 
 				&:hover {
+					transform: translateX(-3px) translateY(-3px);
 					filter: drop-shadow(9px 11px 0px #000000);
 					color: black;
 					background-color: white;
@@ -104,18 +131,20 @@ const Hero = styled.main`
 			img {
 				transition: all 0.1s ease-out;
 				box-sizing: border-box;
-				border: 0.5rem solid black;
-
-				&:hover {
-					cursor: pointer;
-					border: 0.5rem solid white;
-					transform: translateX(-5px) translateY(-5px);
-					filter: drop-shadow(20px 20px 0px #000000);
-				}
+				border: 1rem solid #ffed49;
+				outline: 0.3rem solid black;
+				border-radius: 1rem;
 				width: 100%;
 				height: 100%;
 				object-fit: cover;
-				border-radius: 1rem;
+				filter: drop-shadow(5px 5px 0px #000000);
+
+				&:hover {
+					cursor: pointer;
+					border-color: white;
+					transform: translateX(-5px) translateY(-5px);
+					filter: drop-shadow(20px 20px 0px black);
+				}
 			}
 		}
 	}
@@ -157,7 +186,12 @@ function App() {
 						{/* HERO RIGHT */}
 						<div className='hero-grid-right'>
 							<div className='hero-img-wrapper'>
-								<img src={HeroImage} alt='' />
+								<img
+									src={
+										"https://www.foodandwine.com/thmb/DI29Houjc_ccAtFKly0BbVsusHc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/crispy-comte-cheesburgers-FT-RECIPE0921-6166c6552b7148e8a8561f7765ddf20b.jpg"
+									}
+									alt=''
+								/>
 							</div>
 						</div>
 					</div>
